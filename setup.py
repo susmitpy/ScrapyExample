180792080f997df86cac20c78e9244e6e1743d1e
Susmit Vengurlekar <susmit.py@gmail.com>
2021-05-13 18:06:34 +0530

    setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name="ScrapyCodeGenExample", 
     version="0.0.1",
-    description="A scrapy code generator code with an example",
+    description="A scrapy code generator with an example",
 )
